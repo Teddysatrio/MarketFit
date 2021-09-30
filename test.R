6752b94de10c4c33ef91ff6ec8ab156331bbbfbb
Teddysatrio <gendutgila.kosayu1@gmail.com>
2021-09-30 18:00:59 +0700

    update without function still error in input validation

--- test.R
+++ test.R
@@ -58,11 +58,8 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                     uiOutput('table'),
                     
                     h4("Result Output 2"),
-                    textOutput('result')
+                    uiOutput('result')
                   ) # mainPanel
-                  
-                  
-                  
                 ) # navbarPage
 ) # fluidPage
 
@@ -92,7 +89,7 @@ server <- function(input, output) {
     OC_5 <- OC %>% filter(Package.Price <=20000 & Category.Business =="Voice")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
     OC_6 <- OC %>% filter(Package.Price <=20000 & Category.Business =="Sms")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
     OC_49 <- OC %>% filter(Package.Price <=20000 & Category.Business =="Bonus/promotion")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
-
+    
     OC_7 <- OC %>% filter(Package.Price >20000 & Package.Price <=50000 & Category.Business =="Validity + quota")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
     OC_8 <- OC %>% filter(Package.Price >20000 & Package.Price <=50000 & Category.Business =="Roaming")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
     OC_9 <- OC %>% filter(Package.Price >20000 & Package.Price <=50000 & Category.Business =="VAS")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
@@ -149,174 +146,167 @@ server <- function(input, output) {
     OC_48 <- OC %>% filter(Package.Price >400000 & Category.Business =="Sms")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
     OC_56 <- OC %>% filter(Package.Price >400000 & Category.Business =="Bonus/promotion")%>% select(c(Package.Price, Package.Validity, Value.added.Service.VAS.., SOP))
     
-    Market_fit <- function(SF){
-      #look_for <- c(name)
-      #a <- tail(SF[SF$package_name_detail %in% look_for, ],1)
-      #print(a)
-      a<- tail(SF,1)
-      if(a$package_price <=20000 & a$category_business == "Validity + quota"){
-        b = OC_1
-      }else if(a$package_price <=20000 & a$category_business =="Roaming"){
-        b = OC_2
-      }else if(a$package_price <=20000 & a$category_business =="VAS"){
-        b = OC_3
-      }else if(a$package_price <=20000 & a$category_business =="Unlimited"){
-        b = OC_4
-      }else if(a$package_price <=20000 & a$category_business =="Voice"){
-        b = OC_5
-      }else if(a$package_price <=20000 & a$category_business =="Sms"){
-        b = OC_6
-      }else if(a$package_price <=20000 & a$category_business =="Bonus/promotion"){
-        b = OC_49
-      }else if(a$package_price >20000 & a$package_price <=50000 & a$category_business =="Validity + quota"){
-        b = OC_7
-      }else if(a$package_price >20000 & a$package_price <=50000 & a$category_business =="Roaming"){
-        b = OC_8
-      }else if(a$package_price >20000 & a$package_price <=50000 & a$category_business =="VAS"){
-        b = OC_9
-      }else if(a$package_price >20000 & a$package_price <=50000 & a$category_business =="Unlimited"){
-        b = OC_10
-      }else if(a$package_price >20000 & a$package_price <=50000 & a$category_business =="Voice"){
-        b = OC_11
-      }else if(a$package_price >20000 & a$package_price <=50000 & a$category_business =="Sms"){
-        b = OC_12
-      }else if(a$package_price >20000 & a$package_price <=50000 & a$category_business =="Bonus/promotion"){
-        b = OC_50
-      }else if(a$package_price >50000 & a$package_price <=75000 & a$category_business =="Validity + quota"){
-        b = OC_13
-      }else if(a$package_price >50000 & a$package_price <=75000 & a$category_business =="Roaming"){
-        b = OC_14
-      }else if(a$package_price >50000 & a$package_price <=75000 & a$category_business =="VAS"){
-        b = OC_15
-      }else if(a$package_price >50000 & a$package_price <=75000 & a$category_business =="Unlimited"){
-        b = OC_16
-      }else if(a$package_price >50000 & a$package_price <=75000 & a$category_business =="Voice"){
-        b = OC_17
-      }else if(a$package_price >50000 & a$package_price <=75000 & a$category_business =="Sms"){
-        b = OC_18
-      }else if(a$package_price >50000 & a$package_price <=75000 & a$category_business =="Bonus/promotion"){
-        b = OC_51
-      }else if(a$package_price >75000 & a$package_price <=100000 & a$category_business =="Validity + quota"){
-        b = OC_19
-      }else if(a$package_price >75000 & a$package_price <=100000 & a$category_business =="Roaming"){
-        b = OC_20
-      }else if(a$package_price >75000 & a$package_price <=100000 & a$category_business =="VAS"){
-        b = OC_21
-      }else if(a$package_price >75000 & a$package_price <=100000 & a$category_business =="Unlimited"){
-        b = OC_22
-      }else if(a$package_price >75000 & a$package_price <=100000 & a$category_business =="Voice"){
-        b = OC_23
-      }else if(a$package_price >75000 & a$package_price <=100000 & a$category_business =="Sms"){
-        b = OC_24
-      }else if(a$package_price >75000 & a$package_price <=100000 & a$category_business =="Bonus/promotion"){
-        b = OC_52
-      }else if(a$package_price >100000 & a$package_price <=150000 & a$category_business =="Validity + quota"){
-        b = OC_25
-      }else if(a$package_price >100000 & a$package_price <=150000 & a$category_business =="Roaming"){
-        b = OC_26
-      }else if(a$package_price >100000 & a$package_price <=150000 & a$category_business =="VAS"){
-        b = OC_27
-      }else if(a$package_price >100000 & a$package_price <=150000 & a$category_business =="Unlimited"){
-        b = OC_28
-      }else if(a$package_price >100000 & a$package_price <=150000 & a$category_business =="Voice"){
-        b = OC_29
-      }else if(a$package_price >100000 & a$package_price <=150000 & a$category_business =="Sms"){
-        b = OC_30
-      }else if(a$package_price >100000 & a$package_price <=150000 & a$category_business =="Bonus/promotion"){
-        b = OC_53
-      }else if(a$package_price >150000 & a$package_price <=200000 & a$category_business =="Validity + quota"){
-        b = OC_31
-      }else if(a$package_price >150000 & a$package_price <=200000 & a$category_business =="Roaming"){
-        b = OC_32
-      }else if(a$package_price >150000 & a$package_price <=200000 & a$category_business =="VAS"){
-        b = OC_33
-      }else if(a$package_price >150000 & a$package_price <=200000 & a$category_business =="Unlimited"){
-        b = OC_34
-      }else if(a$package_price >150000 & a$package_price <=200000 & a$category_business =="Voice"){
-        b = OC_35
-      }else if(a$package_price >150000 & a$package_price <=200000 & a$category_business =="Sms"){
-        b = OC_36
-      }else if(a$package_price >150000 & a$package_price <=200000 & a$category_business =="Bonus/promotion"){
-        b = OC_54
-      }else if(a$package_price >200000 & a$package_price <=400000 & a$category_business =="Validity + quota"){
-        b = OC_37
-      }else if(a$package_price >200000 & a$package_price <=400000 & a$category_business =="Roaming"){
-        b = OC_38
-      }else if(a$package_price >200000 & a$package_price <=400000 & a$category_business =="VAS"){
-        b = OC_39
-      }else if(a$package_price >200000 & a$package_price <=400000 & a$category_business =="Unlimited"){
-        b = OC_40
-      }else if(a$package_price >200000 & a$package_price <=400000 & a$category_business =="Voice"){
-        b = OC_41
-      }else if(a$package_price >200000 & a$package_price <=400000 & a$category_business =="Sms"){
-        b = OC_42
-      }else if(a$package_price >200000 & a$package_price <=400000 & a$category_business =="Bonus/promotion"){
-        b = OC_55
-      }else if(a$package_price >400000 & a$category_business =="Validity + quota"){
-        b = OC_43
-      }else if(a$package_price >400000 & a$category_business =="Roaming"){
-        b = OC_44
-      }else if(a$package_price >400000 & a$category_business =="VAS"){
-        b = OC_45
-      }else if(a$package_price >400000 & a$category_business =="Unlimited"){
-        b = OC_46
-      }else if(a$package_price >400000 & a$category_business =="Voice"){
-        b = OC_47
-      }else if(a$package_price >400000 & a$category_business =="Sms"){
-        b = OC_48
-      }else if(a$package_price >400000 & a$category_business =="Bonus/promotion"){
-        b = OC_56
+    if(input$package_price <=20000 & input$category_business == "Validity + quota"){
+      b = OC_1
+    }else if(input$package_price <=20000 & input$category_business =="Roaming"){
+      b = OC_2
+    }else if(input$package_price <=20000 & input$category_business =="VAS"){
+      b = OC_3
+    }else if(input$package_price <=20000 & input$category_business =="Unlimited"){
+      b = OC_4
+    }else if(input$package_price <=20000 & input$category_business =="Voice"){
+      b = OC_5
+    }else if(input$package_price <=20000 & input$category_business =="Sms"){
+      b = OC_6
+    }else if(input$package_price <=20000 & input$category_business =="Bonus/promotion"){
+      b = OC_49
+    }else if(input$package_price >20000 & input$package_price <=50000 & input$category_business =="Validity + quota"){
+      b = OC_7
+    }else if(input$package_price >20000 & input$package_price <=50000 & input$category_business =="Roaming"){
+      b = OC_8
+    }else if(input$package_price >20000 & input$package_price <=50000 & input$category_business =="VAS"){
+      b = OC_9
+    }else if(input$package_price >20000 & input$package_price <=50000 & input$category_business =="Unlimited"){
+      b = OC_10
+    }else if(input$package_price >20000 & input$package_price <=50000 & input$category_business =="Voice"){
+      b = OC_11
+    }else if(input$package_price >20000 & input$package_price <=50000 & input$category_business =="Sms"){
+      b = OC_12
+    }else if(input$package_price >20000 & input$package_price <=50000 & input$category_business =="Bonus/promotion"){
+      b = OC_50
+    }else if(input$package_price >50000 & input$package_price <=75000 & input$category_business =="Validity + quota"){
+      b = OC_13
+    }else if(input$package_price >50000 & input$package_price <=75000 & input$category_business =="Roaming"){
+      b = OC_14
+    }else if(input$package_price >50000 & input$package_price <=75000 & input$category_business =="VAS"){
+      b = OC_15
+    }else if(input$package_price >50000 & input$package_price <=75000 & input$category_business =="Unlimited"){
+      b = OC_16
+    }else if(input$package_price >50000 & input$package_price <=75000 & input$category_business =="Voice"){
+      b = OC_17
+    }else if(input$package_price >50000 & input$package_price <=75000 & input$category_business =="Sms"){
+      b = OC_18
+    }else if(input$package_price >50000 & input$package_price <=75000 & input$category_business =="Bonus/promotion"){
+      b = OC_51
+    }else if(input$package_price >75000 & input$package_price <=100000 & input$category_business =="Validity + quota"){
+      b = OC_19
+    }else if(input$package_price >75000 & input$package_price <=100000 & input$category_business =="Roaming"){
+      b = OC_20
+    }else if(input$package_price >75000 & input$package_price <=100000 & input$category_business =="VAS"){
+      b = OC_21
+    }else if(input$package_price >75000 & input$package_price <=100000 & input$category_business =="Unlimited"){
+      b = OC_22
+    }else if(input$package_price >75000 & input$package_price <=100000 & input$category_business =="Voice"){
+      b = OC_23
+    }else if(input$package_price >75000 & input$package_price <=100000 & input$category_business =="Sms"){
+      b = OC_24
+    }else if(input$package_price >75000 & input$package_price <=100000 & input$category_business =="Bonus/promotion"){
+      b = OC_52
+    }else if(input$package_price >100000 & input$package_price <=150000 & input$category_business =="Validity + quota"){
+      b = OC_25
+    }else if(input$package_price >100000 & input$package_price <=150000 & input$category_business =="Roaming"){
+      b = OC_26
+    }else if(input$package_price >100000 & input$package_price <=150000 & input$category_business =="VAS"){
+      b = OC_27
+    }else if(input$package_price >100000 & input$package_price <=150000 & input$category_business =="Unlimited"){
+      b = OC_28
+    }else if(input$package_price >100000 & input$package_price <=150000 & input$category_business =="Voice"){
+      b = OC_29
+    }else if(input$package_price >100000 & input$package_price <=150000 & input$category_business =="Sms"){
+      b = OC_30
+    }else if(input$package_price >100000 & input$package_price <=150000 & input$category_business =="Bonus/promotion"){
+      b = OC_53
+    }else if(input$package_price >150000 & input$package_price <=200000 & input$category_business =="Validity + quota"){
+      b = OC_31
+    }else if(input$package_price >150000 & input$package_price <=200000 & input$category_business =="Roaming"){
+      b = OC_32
+    }else if(input$package_price >150000 & input$package_price <=200000 & input$category_business =="VAS"){
+      b = OC_33
+    }else if(inpu$package_price >150000 & input$package_price <=200000 & input$category_business =="Unlimited"){
+      b = OC_34
+    }else if(input$package_price >150000 & input$package_price <=200000 & input$category_business =="Voice"){
+      b = OC_35
+    }else if(input$package_price >150000 & input$package_price <=200000 & input$category_business =="Sms"){
+      b = OC_36
+    }else if(input$package_price >150000 & input$package_price <=200000 & input$category_business =="Bonus/promotion"){
+      b = OC_54
+    }else if(input$package_price >200000 & input$package_price <=400000 & input$category_business =="Validity + quota"){
+      b = OC_37
+    }else if(input$package_price >200000 & input$package_price <=400000 & input$category_business =="Roaming"){
+      b = OC_38
+    }else if(input$package_price >200000 & input$package_price <=400000 & input$category_business =="VAS"){
+      b = OC_39
+    }else if(input$package_price >200000 & input$package_price <=400000 & input$category_business =="Unlimited"){
+      b = OC_40
+    }else if(input$package_price >200000 & input$package_price <=400000 & input$category_business =="Voice"){
+      b = OC_41
+    }else if(input$package_price >200000 & input$package_price <=400000 & input$category_business =="Sms"){
+      b = OC_42
+    }else if(input$package_price >200000 & input$package_price <=400000 & input$category_business =="Bonus/promotion"){
+      b = OC_55
+    }else if(input$package_price >400000 & input$category_business =="Validity + quota"){
+      b = OC_43
+    }else if(input$package_price >400000 & input$category_business =="Roaming"){
+      b = OC_44
+    }else if(input$package_price >400000 & input$category_business =="VAS"){
+      b = OC_45
+    }else if(input$package_price >400000 & input$category_business =="Unlimited"){
+      b = OC_46
+    }else if(input$package_price >400000 & input$category_business =="Voice"){
+      b = OC_47
+    }else if(input$package_price >400000 & input$category_business =="Sms"){
+      b = OC_48
+    }else if(input$package_price >400000 & input$category_business =="Bonus/promotion"){
+      b = OC_56
+    }
+    if(#dim(b)[1]==0 #using dimension
+      length(b[,1])==0){#using length
+      paste("There are No Similiar Competitor Package")
+    }else{
+      total_poin = 0;
+      for(i in 1: length(b[,1])){
+        temp_poin = 0
+        #print(b[i,]$Package.Price)
+        #print(length(b$Package.Price))
+        if(input$package_price <= b[i,]$Package.Price){
+          temp_poin = temp_poin+1
+        }
+        else{
+          temp_poin = temp_poin+0
+        }
+        if(input$package_validity >= b[i,]$Package.Validity){
+          temp_poin = temp_poin+1
+        }
+        if(input$vas=="Yes"){
+          temp_poin = temp_poin +1
+        }else if(inputvas=="No" & b[i,]$Value.added.Service.VAS..=="No"){
+          temp_poin = temp_poin +1
+        }
+        if(input$SOP == b[i,]$SOP){
+          temp_poin = temp_poin +1
+        }else if(input$SOP == 6){
+          temp_poin = temp_poin +1
+        }else if(input$SOP == 3 & b[i,]$SOP ==2){
+          temp_poin = temp_poin +1
+        }else if(input$SOP == 2 & b[i,]$SOP ==1){
+          temp_poin = temp_poin +1
+        }else if(input$SOP == 3 & b[i,]$SOP ==1){
+          temp_poin = temp_poin +1
+        }else if(input$SOP != b[i,]$SOP & b[i,]$SOP !=6){
+          temp_poin = temp_poin +1
+        }
+        if(temp_poin > 2){
+          total_poin = total_poin +1
+        }
       }
-      #print(b)
-      if(#dim(b)[1]==0 #using dimension
-        length(b[,1])==0){#using length
-        return("There are No Similiar Competitor Package")
-      }else{
-        total_poin = 0;
-        for(i in 1: length(b[,1])){
-          temp_poin = 0
-          #print(b[i,]$Package.Price)
-          #print(length(b$Package.Price))
-          if(a$package_price <= b[i,]$Package.Price){
-            temp_poin = temp_poin+1
-          }
-          else{
-            temp_poin = temp_poin+0
-          }
-          if(a$package_validity >= b[i,]$Package.Validity){
-            temp_poin = temp_poin+1
-          }
-          if(a$vas=="Yes"){
-            temp_poin = temp_poin +1
-          }else if(a$vas=="No" & b[i,]$Value.added.Service.VAS..=="No"){
-            temp_poin = temp_poin +1
-          }
-          if(a$SOP == b[i,]$SOP){
-            temp_poin = temp_poin +1
-          }else if(a$SOP == 6){
-            temp_poin = temp_poin +1
-          }else if(a$SOP == 3 & b[i,]$SOP ==2){
-            temp_poin = temp_poin +1
-          }else if(a$SOP == 2 & b[i,]$SOP ==1){
-            temp_poin = temp_poin +1
-          }else if(a$SOP == 3 & b[i,]$SOP ==1){
-            temp_poin = temp_poin +1
-          }else if(a$SOP != b[i,]$SOP & b[i,]$SOP !=6){
-            temp_poin = temp_poin +1
-          }
-          if(temp_poin > 2){
-            total_poin = total_poin +1
-          }
-        }
-        #print()
-        if(total_poin / length(b[,1])>= 0.5){
-          return("Fit to the market")
-        }else if(total_poin/length(b[,1])<0.5){
-          return("Not fit to the market")
-        }
+      #print()
+      if(total_poin / length(b[,1])>= 0.5){
+        paste("Fit to the market")
+      }else if(total_poin/length(b[,1])<0.5){
+        paste("Not fit to the market")
       }
     }
-    paste(Market_fit(SF))
+    
   }) 
   
   output$result <- renderText({
